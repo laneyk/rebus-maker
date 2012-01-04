145e8e5bcad014eba372e0b56e3dd65f42c90855
Laney Kuenzel <laney.kuenzel@gmail.com>
2012-01-03 22:31:57 -0500

    accounted for different image filetypes

--- imagesave.php
+++ imagesave.php
@@ -1,8 +1,24 @@
 <?php
-  
+ 
   $url = $_GET['url'];
+  $ext = substr($url, -3);
+  switch ($ext) {
+    case 'jpg':
+      $mime = 'image/jpeg';
+      break;
+    case 'gif':
+      $mime = 'image/gif';
+      break;
+    case 'png':
+      $mime = 'image/png';
+      break;
+    default:
+      $mime = false;
+  }
   $contents = file_get_contents($url);
-  header('Content-type: image/jpeg');
+  if ($mime) {
+    header('Content-type: '.$mime);
+  }
   echo $contents;
 ?>
 
